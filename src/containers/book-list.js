593926e559fa966c7950686e9ad88f9639bc0e6f
moneer-alasmar <kiraalasmar@gmail.com>
2018-11-10 14:42:15 -0800

    completed booklist container

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 
-export default class BookList extends Component {
+class BookList extends Component {
   renderList() {
     return this.props.books.map(book => {
       return (
@@ -10,7 +11,17 @@ export default class BookList extends Component {
       );
     });
   }
+
   render() {
     return <ul className="list-group col-sm-4">{this.renderList()}</ul>;
   }
 }
+
+function mapStateToProps(state) {
+  // WHATEVER IS RETURNED FROM APPLICATION STATE WILL SHOW UP AS PROPS FOR OUR CONTAINER, IN THIS CASE OUR BOOK LIST CONTAINER WILL HAVE ACCESS TO OUR LIST OF BOOKS IN APPLICATION STATE
+  return {
+    books: state.books
+  };
+}
+
+export default connect(mapStateToProps)(BookList);
